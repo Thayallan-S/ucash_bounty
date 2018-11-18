48ad5f47fa44f94b2765e8da512c2197e09a7297
jadenbh12 <jadenbh12@gmail.com>
2018-11-18 09:33:47 -0500

    Made UI better

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ucash_bounty
-A project for DMZ BlockHack 2018. A bounty system built for Ucash.
--- public/src_hello.js
+++ public/src_hello.js
@@ -3,18 +3,26 @@ $(document).ready(function() {
 $("#response").hide()
 $("#submit").hide()
 $("#urRes").hide()
+$("#inputStringe").hide()
+$("#finalButton").hide()
 })
 var array1 = []
 var array2 = []
+var bount = 0
+
 $("#oneButton").click(function() {
-send.searchResponsePerQuestion(function(err, res) {
+send.searchResponsePerQuestion(web3.eth.defaultAccount, function(err, res) {
     if(err) {
     console.log(err)
     } else {
     $("#urRes").show()
+    $("#inputString").show()
+    $("#inputStringe").show()
+
+$("#finalButton").show()
     secondArray = res
     for (var j = 0; j < secondArray.length; j++) {
-        send.searchResponseByNumber(1, function(err, res) {
+        send.searchResponseByNumber(2, function(err, res) {
             if(err) {
                 console.log(err)
             } else {
@@ -47,10 +55,10 @@ send.searchResponsePerQuestion(function(err, res) {
          web3.eth.defaultAccount = web3.eth.accounts[0]
          console.log(web3.eth.defaultAccount)
 
-var sendContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"addresses","type":"address[]"}],"name":"acceptArray2","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"approval","type":"bool"},{"name":"_selectedAddress","type":"address"}],"name":"approveOfResponse","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"returnArray","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_response","type":"string"},{"name":"_questionAddress","type":"address"}],"name":"respondToQuestion","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"returnMsg","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"questions","outputs":[{"name":"contents","type":"string"},{"name":"bounty","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"uints","type":"uint256[]"},{"name":"number","type":"uint256"}],"name":"acceptArray","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"returnAllAddresses","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"searchResponsePerQuestion","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"users","outputs":[{"name":"respondant","type":"address"},{"name":"approval","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_responseNumber","type":"uint256"}],"name":"searchResponseByNumber","outputs":[{"name":"","type":"string"},{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_question","type":"string"},{"name":"_bounty","type":"uint256"}],"name":"postQuestion","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"addresses","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"returnAllBounties","outputs":[{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]);
+var sendContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"addresses","type":"address[]"}],"name":"acceptArray2","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"approval","type":"bool"},{"name":"_selectedAddress","type":"address"}],"name":"approveOfResponse","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"returnArray","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_response","type":"string"},{"name":"_questionAddress","type":"address"}],"name":"respondToQuestion","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"returnMsg","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowed","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"questions","outputs":[{"name":"contents","type":"string"},{"name":"bounty","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"searchResponsePerQuestion","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"uints","type":"uint256[]"},{"name":"number","type":"uint256"}],"name":"acceptArray","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"returnAllAddresses","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"users","outputs":[{"name":"respondant","type":"address"},{"name":"approval","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_responseNumber","type":"uint256"}],"name":"searchResponseByNumber","outputs":[{"name":"","type":"string"},{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_question","type":"string"},{"name":"_bounty","type":"uint256"}],"name":"postQuestion","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"addresses","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"returnAllBounties","outputs":[{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]);
 
 var mainAddress
-    var send = sendContract.at("0x1e8bc1d0450f416f98daa8d26fb4a2dbae242fda")
+    var send = sendContract.at("0x002e3f8e805ee52e0b1f1ba765e8e8ecf1368780")
     console.log(send)
     send.returnMsg(function(err, res) {
     if(err) {
@@ -88,7 +96,6 @@ $("#submit").click(function() {
 	})
 })
 send.returnAllAddresses(function(err, res) {
-    var bount = 0
 	if(err) {
 	alert("Something went wrong")
 	} else {
@@ -105,7 +112,7 @@ send.returnAllAddresses(function(err, res) {
 	bountyArray.push(result[1].c)
 	console.log(bountyArray)
     numberArray.push(bount)
-
+    console.log(bount)
     var text = "";
     for(var k = 0; k < messageArray.length; k++) {
         text += messageArray[k] + "<br>";
@@ -125,19 +132,16 @@ send.returnAllAddresses(function(err, res) {
     var bnd = $("#numberVal").val()
     console.log(bnd - 1)
     send.acceptArray(bountyArray, bnd-1, function(err, res) {
-        if(err) {
-            console.log(err)
-        } else {
+        if(!err) {
             console.log(res)
-        }
+        } 
     })
     send.acceptArray2(array, function(err, res) {
-        if(err) {
-            console.log(err)
-        } else {
+        if(!err) {
             console.log(res)
         }
     })
+    window.location.replace('./bounty.html')
     })
 	}
 	})
@@ -147,5 +151,19 @@ send.returnAllAddresses(function(err, res) {
     $(".clickable-row").click(function() {
         window.location.assign("./bounty.html");
     });
+$("#finalButton").click(function() {
+    if(web3.eth.defaultAccount == 0x3e53850cf23cd51254a134c77a2deba0fdd274b6) {
+    send.approveOfResponse(true, $("#inputStringe").val(), function(err, res) {
+        if(err) {
+            console.log(err)
+        } else {
+            alert("Transaction sent with hash: " + res)
+            console.log(res)
+        }
+    })
+    } else {
+        console.log("Scammer alert")
+    }
+})
 
     
